e515f046bae6b1fb385ed0f57886c105e1b16bcc
Agus Mc Innes <agustinmcinnes@gmail.com>
2024-10-07 20:04:58 -0300

    Cambios para el iphone

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           entry.target.classList.remove('animate');
         }
       });
-    }, { threshold: 0.1 });
+    });
 
     const observerGaleria = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
